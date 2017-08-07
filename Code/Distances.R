9813373edeeb835a6ef72dd60922f9f53e9512ba
tomszar <tomasgzarzar@gmail.com>
2017-08-06 22:11:56 -0400

    added functions to obtain angle between two or more vectors

--- Code/Distances.R
+++ Code/Distances.R
@@ -12,3 +12,37 @@ getDistance <- function(face1, face2)
   
   return(eucdist)
 }
+
+getAngle <- function(x1, x2)
+{
+  #Computes the angle between two vectors, x1 and x2
+  
+  x1 <- as.numeric(x1)
+  x2 <- as.numeric(x2)
+  dot.prod <- x1 %*% x2 
+  norm.x1  <- norm(x1, type="2")
+  norm.x2  <- norm(x2, type="2")
+  theta    <- acos(dot.prod / (norm.x1 * norm.x2))
+  theta    <- round(as.numeric(theta), digits = 4)
+  return(theta)
+}
+
+getPAngle <- function(x)
+{
+  #Computes pairwise angles from matrix x,
+  #were each row is a vector
+  
+  x <- as.matrix(x)
+  ptheta <- matrix(ncol = nrow(x), nrow = nrow(x))
+  for(r in 1:nrow(x))
+  {
+    for(r2 in 1:nrow(x))
+    {
+      angle <- getAngle(x[r,], x[r2,])
+      ptheta[r,r2] <- angle
+    }
+    
+  }
+  ptheta[is.nan(ptheta)] <- 0
+  return(ptheta)
+}
