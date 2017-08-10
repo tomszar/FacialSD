ac470193adcce37ec4214520a7e285a470d1c645
tomszar <tomasgzarzar@gmail.com>
2017-08-10 12:39:11 -0400

    removed hover info from plots

--- Code/PlotFaces.R
+++ Code/PlotFaces.R
@@ -26,17 +26,19 @@ Plot2Faces <- function(face1, face2, facets, title = NULL )
         y = -0.1, 
         z = 1.7)))
   
-  facecolor1 <- rep("turquoise", nrow(facets)) 
-  facecolor2 <- rep("wheat", nrow(facets))
+  facecolor1 <- rep("turquoise", nrow(facets))  
+  facecolor2 <- rep("wheat", nrow(facets)) 
   
   myPlot <- plot_ly() %>% 
     add_trace(data = rface1,  x=rface1[,1], y=rface1[,2], z=rface1[,3], i = facets[, 1]-1, j = facets[, 2]-1, k = facets[, 3]-1, 
               type="mesh3d", opacity=0.5, flatshading=F, facecolor = facecolor1,
-              lightposition=list(x=100000, y=10000, z=100), lighting=list(ambient=0.6, specular=0.1, diffuse=0.5) ) %>%
+              lightposition=list(x=100000, y=10000, z=100), lighting=list(ambient=0.6, specular=0.1, diffuse=0.5),
+              hoverinfo = "none") %>%
     
     add_trace(data = rface2,  x=rface2[,1], y=rface2[,2], z=rface2[,3], i = facets[, 1]-1, j = facets[, 2]-1, k = facets[, 3]-1, 
               type="mesh3d", opacity=0.5, flatshading=F, facecolor = facecolor2,
-              lightposition=list(x=100000, y=10000, z=100), lighting=list(ambient=0.6, specular=0.1, diffuse=0.5) ) %>%
+              lightposition=list(x=100000, y=10000, z=100), lighting=list(ambient=0.6, specular=0.1, diffuse=0.5),
+              hoverinfo = "none") %>%
     layout(scene = scene, title = title)
   return(myPlot)
 }
@@ -69,11 +71,12 @@ Plot1Face <- function(face1, facets, colormap=NULL, title=NULL)
         z = 1.7)))  
   
   if(is.null(colormap)){
-    facecolor1 <- rep("turquoise", nrow(facets)) 
+    facecolor1 <- rep("turquoise", nrow(facets))
     myPlot <- plot_ly(
       x = rface1[,1], y = rface1[,2], z = rface1[,3],
       i = facets[, 1]-1, j = facets[, 2]-1, k = facets[, 3]-1,
       facecolor = facecolor1, opacity = 0.8,
+      hoverinfo = "none",
       type = "mesh3d"
     ) %>% 
       layout(scene = scene, title = title)
@@ -87,6 +90,7 @@ Plot1Face <- function(face1, facets, colormap=NULL, title=NULL)
       #                  c(1, 'rgb(0, 0, 255)')),
       colors = colorRamp(c("yellow", "red", "black")), 
       intensity = colormap, opacity = 1,
+      hoverinfo = "none",
       type = "mesh3d"
     ) %>% 
       layout(scene = scene, title = title)
