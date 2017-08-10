ac2a3486d873d13f6a399471d5492bed9ece796d
tomszar <tomasgzarzar@gmail.com>
2017-08-09 20:33:06 -0400

    added functions for cosine similarity, and null distribution by permutation

--- Code/Distances.R
+++ Code/Distances.R
@@ -13,6 +13,17 @@ getDistance <- function(face1, face2)
   return(eucdist)
 }
 
+getGDistance <- function(matrix, grouping)
+{
+  #Compute the euclidean distance between groups 
+  
+  require(dplyr)
+  averages   <- matrix %>% group_by(as.character(grouping)) %>% summarise_all(funs(mean))
+  distmatrix <- dist(averages[,2:ncol(averages)])
+  return(distmatrix)
+  
+}
+
 getAngle <- function(x1, x2)
 {
   #Computes the angle between two vectors, x1 and x2
@@ -27,22 +38,90 @@ getAngle <- function(x1, x2)
   return(theta)
 }
 
-getPAngle <- function(x)
+getPAngle <- function(X)
 {
-  #Computes pairwise angles from matrix x,
+  #Computes pairwise angles from matrix X,
   #were each row is a vector
   
-  x <- as.matrix(x)
-  ptheta <- matrix(ncol = nrow(x), nrow = nrow(x))
-  for(r in 1:nrow(x))
+  X <- as.matrix(X)
+  ptheta <- matrix(ncol = nrow(X), nrow = nrow(X))
+  for(r in 1:nrow(X))
   {
-    for(r2 in 1:nrow(x))
+    for(r2 in 1:nrow(X))
     {
-      angle <- getAngle(x[r,], x[r2,])
+      angle <- getAngle(X[r,], X[r2,])
       ptheta[r,r2] <- angle
     }
     
   }
   ptheta[is.nan(ptheta)] <- 0
   return(ptheta)
+}
+
+getCosine <- function(x1, x2)
+{
+  #Computes the cosine similarity between two vectors
+  x1 <- as.numeric(x1)
+  x2 <- as.numeric(x2)
+  cosine <- sum(x1*x2)/sqrt(sum(x1^2)*sum(x2^2))
+  cosine  <- round(as.numeric(cosine), digits = 4)
+  return(cosine)
+}
+
+getPCosine <- function(X)
+{
+  #Computes pairwise cosine similarities from matrix X
+  #were each row is a vector
+  
+  X <- as.matrix(X)
+  pcos <- matrix(ncol = nrow(X), nrow = nrow(X))
+  for(r in 1:nrow(X))
+  {
+    for(r2 in 1:nrow(X))
+    {
+      cos <- getCosine(X[r,], X[r2,])
+      pcos[r,r2] <- cos
+    }
+    
+  }
+  pcos[is.nan(pcos)] <- 0
+  return(pcos)
+}
+
+getAngleperm <- function(matrix, factor, groups, perm = 1000)
+{
+  #Computes a null distribution of angles to test the significance of groups
+  #Matrix is the set of numerical values, one observation per row.
+  #Factor is the grouping factor to obtain the vector.
+  #Groups is the grouping factor to compare the angles.
+  
+  row     <- 1
+  ngroups <- length(levels(groups))
+  matrix  <- as.data.frame(matrix)
+  nullmatrix <- matrix(ncol = perm, nrow = (ngroups * (ngroups-1) /2) )
+  for(r in 1:(ngroups-1))
+  {
+    for(r2 in 2:ngroups)
+    {
+      logic     <- (groups == levels(groups)[r] | groups == levels(groups)[r2])
+      submatrix <- dplyr::filter(matrix, logic)
+      subgroups <- groups[logic]
+      subfactor <- factor[logic]
+      
+      set.seed(10)
+      nullvals <- replicate(perm,
+                            {
+                              resgroups <- sample(subgroups)
+                              averages  <- submatrix %>% group_by(interaction(subfactor, resgroups)) %>% 
+                                               summarise_all(funs(mean(., na.rm=T)))
+                              vector1   <- averages[1, 2:ncol(averages)] - averages[2, 2:ncol(averages)]
+                              vector2   <- averages[3, 2:ncol(averages)] - averages[4, 2:ncol(averages)]
+                              return(getAngle(vector1, vector2))
+                            }
+      )
+      nullmatrix[row, ] <- nullvals
+      row = row + 1
+    }
+  }
+  return(nullmatrix)
 }
