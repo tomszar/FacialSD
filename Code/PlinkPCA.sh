9145522ade0bce6e8f9ac8aca6f706c5fd762d66
tomszar <tomasgzarzar@gmail.com>
2017-09-08 10:57:06 -0400

    change output path

--- Code/PlinkPCA.sh
+++ Code/PlinkPCA.sh
@@ -3,8 +3,8 @@ cd ../Results/MergeGeno/Merge1000G/
 pwd
 
 #We'll generate a PCA using only the 1000G samples, and proyecting ours
-outpath="$(cd ../../GenClustering ;pwd)"
-./plink.exe --bfile Merge1000Gsamples --pca 50 --pca-cluster-names 0 --within 1000Gmerged.fam --out Clus
+outpath="$(cd ../../GenPCA ;pwd)"
+./plink.exe --bfile Merge1000Gsamples --pca 50 --pca-cluster-names 0 --within 1000Gmerged.fam --out PCA
 #Moving output to new folder
-files=$(ls Clus.*)
+files=$(ls PCA.*)
 mv $files "$outpath"
