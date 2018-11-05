f0c1cf7c4235850f4a0a484de6b87649f3fbee56
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2018-11-05 16:36:59 -0500

    Changed face PCA with sparse DNA samples

--- Code/2018-08-LandmarkProcessing.m
+++ Code/2018-08-LandmarkProcessing.m
@@ -13,8 +13,10 @@ cd(folder.databases);
 load('landmark_matrix.mat');
 load('landmark_ids.mat');
 load('landmark_facets.mat');
-%Loading IDs and groups from pop_struct 
-[ids, groups] = textread ('mclust_class.csv', '%s %f', 'delimiter' , ',');
+%Loading IDs and groups from admixture k6 pop_struct 
+[ids, groups] = textread ('clusters_admix_k6.csv', '%s %f', 'delimiter' , ',');
+ids    = ids(2:end,:);
+groups = groups(2:end,:);
 
 %Retain IDs from pop structure analysis into shape analysis
 %First, remove the string 'PSU' from the landmark_ids file, and remove the starting 0
@@ -36,22 +38,41 @@ clear landmark_matrix;
 group_counts  = accumarray(ic,1);
 group_counts
 
+%Get random sample of 120 per group and run PCA on that
+row = 1
+sample = 120
+random_sample = zeros(size(C)(1) * sample, size(shape_matrix )(2));
+for i = 0:5
+  reduced_sample = shape_matrix(groups == i,:);
+  start = 1 + (i *sample);
+  fin   = row * sample;
+  random_sample(start:fin, :) = reduced_sample(randperm(sample), :);
+  row = row + 1;
+endfor
+%Run PCA on random sample
+[V,S,score,mean_eigenvals,index] = fastPCA(random_sample, 100, 1);
+%Get score to whole dataset
+mu = mean(shape_matrix);
+Xm =  bsxfun(@minus, shape_matrix, mu);
+V     = V(:,1:index);
+total_scores = Xm * V;
+
 %Run PCA with whole sample
-[V,S,score,mean_eigenvals,index] = fastPCA(shape_matrix, 100, 1);
+%[V,S,score,mean_eigenvals,index] = fastPCA(shape_matrix, 100, 1);
 #Looking at the number of relevant PCs
 plot(diag(S),'bo-');
 hold on;
 plot(mean_eigenvals,'ro-');
 hold off;
 %Getting PCA scores for relevant PCs
-mu    = mean(shape_matrix);
-score = score(:,1:index);
-V     = V(:,1:index);
+%mu    = mean(shape_matrix);
+%score = score(:,1:index);
+%V     = V(:,1:index);
 %Eigenvalue is (diag(S).^2)/(n-1)
 
 %Testing accuracy of PCA
 %Calculating euclidean distance between original and PCA transformed faces
-fromPCA   = score * V' + mu;
+fromPCA   = total_scores * V' + mu;
 distances = distancePoints(shape_matrix, fromPCA, 'diag');
 distances_rand = distancePoints(shape_matrix, fromPCA(randperm(size(fromPCA,1)), :), 'diag');
 hist(distances,'bo-');
@@ -80,7 +101,7 @@ cd(folder.save)
 cell2csv("landmark_ids.csv", landmark_ids)
 csvwrite("means.csv", mu')
 csvwrite("cs.csv", cs') 
-csvwrite("scores.csv", score) 
+csvwrite("scores.csv", total_scores) 
 csvwrite("eigenvalues.csv", diag(S) ) 
 csvwrite("eigenvectors.csv", V ) 
 csvwrite("facets.csv", landmark_facets ) 
